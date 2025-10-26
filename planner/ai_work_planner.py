c7d03c1c679d7c5fb4e441a0ed62b96e697a82ab
satheesh-chiploop <pillaisatheesh74@gmail.com>
2025-10-26 15:26:24 -0700

    adding graph update

--- planner/ai_work_planner.py
+++ planner/ai_work_planner.py
@@ -13,7 +13,6 @@ SUPABASE_URL = os.environ.get("SUPABASE_URL") or os.environ.get("NEXT_PUBLIC_SUP
 SUPABASE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY") or os.environ.get("NEXT_PUBLIC_SUPABASE_ANON_KEY")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-from supabase_client import supabase
 
 # Reuse your environment variable pattern
 USE_LOCAL_OLLAMA = os.getenv("USE_LOCAL_OLLAMA", "false").lower() == "true"
